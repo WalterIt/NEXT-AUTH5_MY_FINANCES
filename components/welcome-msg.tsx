05fe7cf0e9fff491c156aa813235cbaa645cdb62
WalterIt <wshiruba@gmail.com>
2024-05-23 10:20:35 -0400

    CREATED: DashboardLayout, Implemented Header & HeaderLogo Components; Implementation of Navigation & Welcome-msg Components. BUG: Installled lucide-react.  APP DO NOT RECOGNIZE Module.

---
+++ components/welcome-msg.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+
+export const WelcomeMsg = () => {
+    // const { user, isLoaded } = useUser();
+  return (
+    <div className="space-y-2 mb-4" >
+        <h2 className="text-2xl lg:text-4xl text-white font-medium">
+            {/* Welcome Back{isLoaded ? ", " : ' '}{user?.firstName} 👏 */}
+        </h2>
+        <p className="text-sm lg:text-base text-[#68d0f3] ">
+            This is your Financial Dashboard Report.
+        </p>
+    </div>
+  )
+}
