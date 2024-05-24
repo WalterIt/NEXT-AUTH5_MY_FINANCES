a3ac8f1bff265c145a7d4487af36531a5608827e
WalterIt <wshiruba@gmail.com>
2024-05-24 17:52:49 -0400

    UPDATED: docker-compose.yml, etc

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -5,12 +5,12 @@ import { migrate } from "drizzle-orm/neon-http/migrator";
 
 config({ path: ".env" });
 
-const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
-const db = drizzle(sql);
+const sql = neon(process.env.NEON_DATABASE_URL!);
+const db1 = drizzle(sql);
 
 const main = async () => {
   try {
-    await migrate(db, { migrationsFolder: "drizzle" });
+    await migrate(db1, { migrationsFolder: "drizzle" });
   } catch (error) {
     console.log("error during migration", error);
     process.exit(1);
