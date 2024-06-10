f67fb5330465175c746c22ef27bff65a5ad06fa5
WalterIt <wshiruba@gmail.com>
2024-06-10 10:02:55 -0400

    IMPLEMENTED: Dashboard chart

--- components/chart.tsx
+++ components/chart.tsx
@@ -38,7 +38,7 @@ export const Chart = ({ data = [] }: Props) => {
     <Card className="border-none drop-shadow-sm">
       <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
         <CardTitle className="text-xl line-clamp-1">Transactions</CardTitle>
-        {/* TODO add select */}
+        
         <Select defaultValue={chartType} onValueChange={onChangeType}>
           <SelectTrigger className="lg:w-auto h-9 rounded-md">
             <SelectValue placeholder="Select chart type" />
